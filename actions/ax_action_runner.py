d63321945fd6c7399f5331f1df148b2d5db4cbf2
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-04-28 19:16:48 +0900

    Added actions to configure the Servers in ServiceGroup

--- actions/ax_action_runner.py
+++ actions/ax_action_runner.py
@@ -8,6 +8,9 @@ class AXActionRunner(BaseAction):
         client = self.login(api_version)
         if client:
             try:
+                # transforms user parameters as needed
+                kwargs = self._transform_params(client, object_path, action, **kwargs)
+
                 _target_obj = self.get_object(client, object_path)
 
                 return (True, getattr(_target_obj, action)(**kwargs))
@@ -19,3 +22,13 @@ class AXActionRunner(BaseAction):
                 return (False, 'The acos_client has no interfaces for %s' % (object_path))
         else:
             return (False, 'Failed to initilaize Client object of acos_client')
+
+    def _transform_params(self, client, object_path, action, **kwargs):
+        if object_path == 'slb.service_group.member' and action == 'create':
+            # transforms status parameter from boolean to the internal constant of acos-client
+            if kwargs['status']:
+                kwargs['status'] = client.slb.UP
+            else:
+                kwargs['status'] = client.slb.DOWN
+
+        return kwargs
